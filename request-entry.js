48f181b87a853211235fbc923b67d49188bba443
Jon Marco Reyes <rjonmarco@gmail.com>
2022-04-28 11:37:26 +0800

    fix issue with url in reqentry.js

--- request-entry.js
+++ request-entry.js
@@ -1,8 +1,9 @@
 const axios = require('axios')
 
 var rfuid = process.argv[2];
+var url = "http://afe47cabd8457497fbc403ec51c1c83a-1958255376.ap-east-1.elb.amazonaws.com:9001"
 // console.log(rfuid);
-axios.post('http://afe47cabd8457497fbc403ec51c1c83a-1958255376.ap-east-1.elb.amazonaws.com:9001//seccam/reqentry',
+axios.post(url + '/seccam/reqentry',
     {
     "rfuid" : `${rfuid}`
     }
